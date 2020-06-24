674b7db39cd8cbe5bebe0c7047dfc03ef6569908
edicury <edisoncuryn@gmail.com>
2020-06-24 16:15:54 -0300

    Version bump

--- src/lib.rs
+++ src/lib.rs
@@ -6,22 +6,4 @@ pub mod structures;
 pub mod utils;
 pub mod globals;
 
-pub fn test() {
-    let mut content = String::new();
-    let mut csv = dataframe::df::read_csv("/Users/edisoncury/Documents/Development/Rust/koala/src/dataset.csv", &mut content, Some(","));
-    let mut df = csv.get_df();
-
-
-
-    println!("df.columns {:?} columns", df.columns);
-
-    let mean = df.mean("x");
-    let mean_str: String =  format!("{}", mean);
-
-    df.fillna("x", mean_str.as_str());
-
-    println!("df x {:?}", df.is_na_col("x"));
-
-}
-
 
